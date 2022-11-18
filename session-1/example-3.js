361782a3d4b8522018e65f69f3ef2a59d7ca11c8
Rachmizard <rachmizard11072000@gmail.com>
2022-11-19 01:14:07 +0700

    fix(session-one): negative case first condition

--- session-1/example-3.js
+++ session-1/example-3.js
@@ -11,71 +11,69 @@ const movie = {
 	minAge: 13,
 };
 
+// function ticketValidation(user, movie) {
+// 	const isAdult = user.age >= movie.minAge;
+// 	const worthiness = user.amount >= movie.ticketPrice;
+
+// 	if (isAdult && worthiness) {
+// 		return console.log(
+// 			`Selamat ${user.name}, kamu berhasil membeli tiket ${movie.name} dengan harga ${movie.ticketPrice}`
+// 		);
+// 	}
+
+// 	if (!isAdult && !worthiness)
+// 		return console.log(
+// 			"Gagal membeli tiket karena emang ga layak dari segi apapun"
+// 		);
+
+// 	if (!isAdult)
+// 		return console.log("Gagal membeli tiket karena belum cukup umur");
+
+// 	if (!worthiness)
+// 		return console.log("Gagal membeli tiket karena uang kurang");
+// }
+
+// ticketValidation(user, movie);
+
+// WITH RETURN DATA TYPE CASE
+
 function ticketValidation(user, movie) {
 	const isAdult = user.age >= movie.minAge;
 	const worthiness = user.amount >= movie.ticketPrice;
 
-	if (isAdult && worthiness) {
-		return console.log(
-			`Selamat ${user.name}, kamu berhasil membeli tiket ${movie.name} dengan harga ${movie.ticketPrice}`
-		);
+	if (isAdult && !worthiness) {
+		return {
+			isValid: false,
+			name: user.name,
+			reason: "Kamu ga punya duit",
+		};
 	}
 
-	if (!isAdult && !worthiness)
-		return console.log(
-			"Gagal membeli tiket karena emang ga layak dari segi apapun"
-		);
+	if (!isAdult && worthiness) {
+		return {
+			isValid: false,
+			name: user.name,
+			reason: "Kamu belum dewasa",
+		};
+	}
 
-	if (!isAdult)
-		return console.log("Gagal membeli tiket karena belum cukup umur");
+	if (!isAdult && !worthiness) {
+		return {
+			isValid: false,
+			name: user.name,
+			reason: "Kamu gak layak",
+		};
+	}
 
-	if (!worthiness)
-		return console.log("Gagal membeli tiket karena uang kurang");
+	return {
+		isValid: true,
+		name: user.name,
+		reason: "",
+	};
 }
 
-ticketValidation(user, movie);
+const check = ticketValidation(user, movie);
 
-// WITH RETURN DATA TYPE CASE
-
-// function ticketValidation(user, movie){
-//     const isAdult = user.age >= movie.minAge;
-//     const worthiness = user.amount >= movie.ticketPrice;
-
-//     if(isAdult && worthiness) {
-//       return {
-//       	isValid: true,
-// 				name: user.name,
-//         reason: ''
-//       }
-//     }
-
-//     if(isAdult && !worthiness) {
-//       return {
-//           isValid: false,
-//           name: user.name,
-//           reason: 'Kamu ga punya duit'
-//        }
-//     }
-
-//     if(!isAdult && worthiness) {
-// 	    return {
-//           isValid: false,
-//           name: user.name,
-//           reason: 'Kamu belum dewasa'
-//       }
-//     }
-
-//     if(!isAdult && !worthiness) {
-// 	    return {
-// 					isValid: false,
-//           name: user.name,
-//           reason: 'Kamu gak layak'
-//       }
-//     }
-// }
-
-// const check = ticketValidation(user, movie);
-
-// if(check.isValid)  {
-// 	console.log(`Selamat ${check.name}`);
-// }
+if (check.isValid) {
+	console.log(`Selamat ${check.name}`);
+}
